f78aaa2c16dbc9bf32da014e7717da524d3bb921
jedijinnora <121700416+jedijinnora@users.noreply.github.com>
2023-01-29 18:47:25 -0500

    map, script edits
    modified maps a bit, changed all scripts to use the common_movement versions

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -174,7 +174,7 @@
 #define VAR_UNUSED_0x409B                                0x409B // Unused Var
 #define VAR_ELITE_4_STATE                                0x409C
 #define VAR_UNUSED_0x409D                                0x409D // Unused Var
-#define VAR_MOSSDEEP_SPACE_CENTER_STAIR_GUARD_STATE      0x409E
+#define VAR_UNUSED_0x409E                                0x409E // Unused Var
 #define VAR_MOSSDEEP_SPACE_CENTER_STATE                  0x409F
 #define VAR_SLATEPORT_HARBOR_STATE                       0x40A0
 #define VAR_UNUSED_0x40A1                                0x40A1 // Unused var
