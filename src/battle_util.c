884258655597ecfce86be5dfd27ca2f1ab3ae7d0
jedijinnora <121700416+jedijinnora@users.noreply.github.com>
2024-12-02 01:01:22 -0500

    removed FLAG_NO_ESCAPE behavior in anticipation of 1.10

--- src/battle_util.c
+++ src/battle_util.c
@@ -502,11 +502,6 @@ bool32 TryRunFromBattle(u32 battler)
         gBattleStruct->runTries++;
     }
 
-    if (FlagGet(FLAG_NO_ESCAPE))
-    {
-        effect = 0;
-    }
-
     if (effect != 0)
     {
         gCurrentTurnActionNumber = gBattlersCount;
